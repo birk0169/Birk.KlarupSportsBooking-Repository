d0219f4eddfe07832d52012b6c89bec47e84eabd
birk0169 <birk0169@CV-PC-U-24>
2018-09-17 14:35:24 +0200

    Added a reservation and union handler

---
+++ Birk.KlarupSportsBooking.BIZ/ReservationHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Birk.KlarupSportsBooking.DAL.EF;
+
+namespace Birk.KlarupSportsBooking.BIZ
+{
+    class ReservationHandler : BaseHandler
+    {
+        //Get
+        /// <summary>
+        /// Returns all reservations from the database in a list
+        /// </summary>
+        /// <returns>All reservation from the database in a list</returns>
+        public List<Reservation> GetAlllReservation()
+        {
+            return Model.Reservations.ToList();
+        }
+
+        //Add
+        public void AddReservationToDB(Reservation reservation)
+        {
+            if (reservation == null)
+            {
+
+            }
+            else
+            {
+                Model.Reservations.Add(reservation);
+                Model.SaveChanges();
+            }
+
+        }
+    }
+}
